08b3c58cdb6974073e9258bb403ed19c97228d7d
Nathy-bajo <bajon7680@gmail.com>
2023-10-30 01:16:02 +0100

    added test

--- lib.rs
+++ lib.rs
@@ -1,4 +1,4 @@
-#![cfg_attr(not(feature = "std"), no_std)]
+#![cfg_attr(not(feature = "std"), no_std, no_main)]
 
 #[ink::contract]
 mod nostr_contract {
@@ -7,9 +7,10 @@ mod nostr_contract {
     pub struct NostrContract {
         owner: AccountId,
         subscriptions: Vec<Subscription>,
-        relayer_stakes: Vec<(AccountId, Balance)>,
-        reports: Vec<Report>,
+        pub relayer_stakes: Vec<(AccountId, Balance)>,
+        pub reports: Vec<Report>,
         next_report_id: u64,
+        challenger: Option<AccountId>,
     }
 
     #[derive(Debug, Clone, scale::Encode, scale::Decode, scale_info::TypeInfo)]
@@ -20,10 +21,10 @@ mod nostr_contract {
 
     #[derive(Debug, Clone, scale::Encode, scale::Decode, scale_info::TypeInfo)]
     pub struct Report {
-        reporter: AccountId,
+        pub reporter: AccountId,
         relayer: AccountId,
-        description: Vec<u8>,
-        challenged: bool,
+        pub description: Vec<u8>,
+        pub challenged: bool,
     }
 
     #[ink(event)]
@@ -44,7 +45,7 @@ mod nostr_contract {
     pub struct Reported {
         reporter: AccountId,
         relayer: AccountId,
-        description: Vec<u8>,
+        pub description: Vec<u8>,
         report_id: u64,
     }
 
@@ -64,6 +65,7 @@ mod nostr_contract {
                 relayer_stakes: Vec::new(),
                 reports: Vec::new(),
                 next_report_id: 1,
+                challenger: None,
             }
         }
 
@@ -72,10 +74,7 @@ mod nostr_contract {
             let caller = self.env().caller();
             assert_ne!(caller, relayer, "You cannot subscribe to yourself");
 
-            self.subscriptions.push(Subscription {
-                relayer,
-                amount,
-            });
+            self.subscriptions.push(Subscription { relayer, amount });
 
             self.env().emit_event(Subscribed {
                 subscriber: caller,
@@ -89,17 +88,18 @@ mod nostr_contract {
             let caller = self.env().caller();
             if let Some(subscription) = self.subscriptions.last() {
                 let (relayer, amount) = (subscription.relayer, subscription.amount);
-        
-                let existing_stake = match self.relayer_stakes.iter_mut().find(|(r, _)| *r == relayer) {
-                    Some((_, s)) => s,
-                    None => {
-                        self.relayer_stakes.push((relayer, 0)); // Add a new stake entry
-                        &mut self.relayer_stakes.last_mut().unwrap().1 // Borrow the mutable reference
-                    }
-                };
-                
+
+                let existing_stake =
+                    match self.relayer_stakes.iter_mut().find(|(r, _)| *r == relayer) {
+                        Some((_, s)) => s,
+                        None => {
+                            self.relayer_stakes.push((relayer, 0)); // Add a new stake entry
+                            &mut self.relayer_stakes.last_mut().unwrap().1 // Borrow the mutable reference
+                        }
+                    };
+
                 *existing_stake += amount;
-        
+
                 self.env().emit_event(Staked {
                     staker: caller,
                     relayer,
@@ -108,19 +108,19 @@ mod nostr_contract {
             }
         }
         #[ink(message)]
-        pub fn report(&mut self, relayer: AccountId, description: Vec<u8>) {
+        pub fn report(&mut self, relayer: AccountId, report_id: u64, reporter: AccountId, description: Vec<u8>) {
             let caller = self.env().caller();
-    
+
             let next_report_id = self.next_report_id;
             self.next_report_id += 1; // Increment the next report ID
-    
+
             self.reports.push(Report {
                 reporter: caller,
                 relayer,
                 description: description.clone(),
                 challenged: false,
             });
-    
+
             self.env().emit_event(Reported {
                 reporter: caller,
                 relayer,
@@ -128,29 +128,96 @@ mod nostr_contract {
                 report_id: next_report_id,
             });
         }
-        
 
         #[ink(message)]
-        pub fn challenge(&mut self, report_id: u64) {
-            let caller = self.env().caller();
-            let report = self
-                .reports
-                .iter_mut()
-                .find(|r| r.reporter == caller && r.challenged == false)
-                .unwrap();
-
-            report.challenged = true;
-
-            self.env().emit_event(Challenged {
-                reporter: caller,
-                report_id,
-            });
+        pub fn set_challenger(&mut self, challenger: AccountId) {
+            // Implement a setter function to set the challenger account
+            self.challenger = Some(challenger);
         }
+
+        #[ink(message)]
+        pub fn challenge(&mut self, report_id: u64, challenger: AccountId) {
+            let caller = self.env().caller();
+    
+            // Check if the caller is the stored challenger account
+            if Some(caller) == self.challenger {
+                let report = self
+                    .reports
+                    .iter_mut()
+                    .find(|r| r.reporter == caller && r.challenged == false)
+                    .unwrap();
+    
+                report.challenged = true;
+    
+                self.env().emit_event(Challenged {
+                    reporter: caller,
+                    report_id,
+                });
+            } else {
+                // Handle unauthorized challenges
+                // For example: Emit an event or revert the transaction
+                // You can implement your specific logic here
+            }
+        }
+    
     }
 }
 
-    #[cfg(test)]
-    mod tests {
-   
+#[cfg(test)]
+mod tests {
+    use crate::nostr_contract::NostrContract;
+
+    use ink::env::{test, DefaultEnvironment};
+
+    use super::*;
+
+    #[ink::test]
+    fn test_subscribe_and_stake() {
+        // Deploy the contract
+        let mut contract = NostrContract::new();
+
+        // Get accounts
+        let accounts = test::default_accounts::<DefaultEnvironment>();
+
+        // Subscribe and stake
+        contract.subscribe(accounts.bob, 100);
+        contract.stake();
+
+        // Get the relayer stakes
+        let relayer_stakes = contract.relayer_stakes;
+        assert_eq!(relayer_stakes.len(), 1);
+        assert_eq!(relayer_stakes[0].0, accounts.bob);
+        assert_eq!(relayer_stakes[0].1, 100);
     }
 
+    #[ink::test]
+    fn test_report_and_challenge() {
+        // Use the test environment to set up test accounts.
+        let accounts = test::default_accounts::<DefaultEnvironment>();
+    
+        // Deploy the contract
+        let mut contract = NostrContract::new();
+
+
+        // Subscribe and stake
+        contract.subscribe(accounts.alice, 100);
+        contract.stake();
+    
+        // Report an issue with one account (e.g., accounts.bob)
+        // contract.report(accounts.bob, vec![1, 2, 3]);
+    
+        // Set the challenger account within the contract
+        contract.set_challenger(accounts.bob);
+    
+        // Challenge the report
+        contract.challenge(1, accounts.bob);
+    
+        // Get the challenged report
+        let reports = &contract.reports;
+        assert_eq!(reports.len(), 1);
+        assert_eq!(reports[0].reporter, accounts.bob); // Ensure the reporter matches the account used for reporting
+        assert_eq!(reports[0].challenged, true);
+    
+    }
+    
+}
